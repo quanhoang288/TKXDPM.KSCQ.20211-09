6cde6e01f349971ef38d36cca6ebb09a67ec8c04
ThangProVjp <thang.nd182629@gmail.com>
2021-12-22 11:54:36 +0700

    Singleton entity manager object

--- bike/src/main/java/ecobike/seeders/UserSeeder.java
+++ bike/src/main/java/ecobike/seeders/UserSeeder.java
@@ -1,5 +1,6 @@
 package ecobike.seeders;
 
+import ecobike.db.DbConnection;
 import ecobike.entity.Bike;
 import ecobike.entity.Dock;
 import ecobike.entity.User;
@@ -16,8 +17,7 @@ public class UserSeeder {
 
     public static void seed() {
 
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityManager entityManager = DbConnection.getEntityManager();
         entityManager.getTransaction().begin();
         for (int i = 0; i < 10; i++) {
             String email = "email" + i + "@gmail.com";
@@ -29,6 +29,7 @@ public class UserSeeder {
                     .email(email)
                     .password(password)
                     .name(name)
+                    .address("From ha dong")
                     .build();
             entityManager.persist(user);
 
