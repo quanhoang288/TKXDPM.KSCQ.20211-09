bb9ffaad01ce97255677fa3d15c244c9bb0a3394
NguyenDucThangNghiemTuc <thang.nd182629@gmail.com>
2021-12-26 11:47:13 +0700

    fake dockId

--- bike/src/main/java/ecobike/view/ViewDockHandler.java
+++ bike/src/main/java/ecobike/view/ViewDockHandler.java
@@ -1,8 +1,8 @@
 package ecobike.view;
 
+import ecobike.controller.RentBikeController;
 import ecobike.utils.Configs;
 import ecobike.view.base.BaseScreenHandler;
-import javafx.fxml.FXML;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,8 +14,11 @@ public class ViewDockHandler extends BaseScreenHandler {
 
     public void viewBikeList() throws IOException {
         //TODO: init controller
-        BaseScreenHandler rentBikeHandler = new RentBikeHandler(this.stage, Configs.RENT_BIKE_PATH);
+        RentBikeHandler<RentBikeController> rentBikeHandler = new RentBikeHandler<>(this.stage, Configs.RENT_BIKE_PATH);
         rentBikeHandler.setPreviousScreen(this);
+
+        rentBikeHandler.setBController(new RentBikeController("e2cf4dca-4ea1-40a3-92c2-6b9c349938c0"));
+        rentBikeHandler.initializeBikes();
         rentBikeHandler.setScreenTitle("Bike List Screen");
         rentBikeHandler.show();
     }
