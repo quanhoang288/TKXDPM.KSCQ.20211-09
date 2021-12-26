25869443ae15b63e6149eab3423f11ab29b5ef4f
NguyenDucThangNghiemTuc <thang.nd182629@gmail.com>
2021-12-26 11:42:48 +0700

    add controller logic

--- bike/src/main/java/ecobike/controller/RentBikeController.java
+++ bike/src/main/java/ecobike/controller/RentBikeController.java
@@ -1,4 +1,24 @@
 package ecobike.controller;
 
-public class RentBikeController {
+import ecobike.controller.base.BaseController;
+import ecobike.entity.Bike;
+import ecobike.repository.BikeRepo;
+
+import java.util.List;
+
+public class RentBikeController extends BaseController {
+    private String dockId;
+
+    public RentBikeController(String dockId) {
+        this.dockId = dockId;
+    }
+
+    public List<Bike> loadBikeFromDB(int start, int count) {
+        assert dockId != null;
+        List<Bike> bikes = BikeRepo.getPagingByDock(start, count, dockId);
+        return bikes;
+
+    }
+
+
 }
