9eed976312388dfa8b1c802d6451b9a4c51f9393
quanhoang288 <quanpro288@gmail.com>
2022-01-03 15:56:33 +0700

    refactor interbank subsystem

--- bike/src/main/java/ecobike/subsystem/InterbankInterface.java
+++ bike/src/main/java/ecobike/subsystem/InterbankInterface.java
@@ -4,12 +4,13 @@ import ecobike.common.exception.PaymentException;
 import ecobike.common.exception.UnrecognizedException;
 import ecobike.entity.CreditCard;
 import ecobike.entity.PaymentTransaction;
+import ecobike.utils.MyMap;
 
 public interface InterbankInterface {
 
-    public abstract PaymentTransaction performPayment(CreditCard card, int amount, String contents)
+    public abstract MyMap performPayment(CreditCard card, int amount, String contents)
             throws PaymentException, UnrecognizedException;
 
-    public abstract PaymentTransaction performRefund(CreditCard card, int amount, String contents)
+    public abstract MyMap performRefund(CreditCard card, int amount, String contents)
             throws PaymentException, UnrecognizedException;
 }
